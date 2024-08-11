e171295c7e348369b24d5e242b678a92b574f130
Amy T <amyt@Amys-MacBook-Pro.local>
2024-08-11 13:16:37 -0400

    adding hotwire

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "hotwire-rails", "~> 0.1.3"
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
+import "@hotwired/turbo-rails";
+import "controllers";
 
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap";
