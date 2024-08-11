574b563fc2b3d281824c67edea7441f9b71e257b
Amy T <amyt@Amys-MacBook-Pro.local>
2024-08-11 15:19:46 -0400

    add devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+
   root to: 'main#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
