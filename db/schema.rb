24e0e3711d89a3a50a1ae06cffe29aebf35dd237
Amy T <amyt@Amys-MacBook-Pro.local>
2024-08-12 14:16:43 -0400

    adding discussions migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_11_172337) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_12_015045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "discussions", force: :cascade do |t|
+    t.string "name"
+    t.boolean "pinned", default: false
+    t.boolean "closed", default: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_discussions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +39,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_11_172337) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "discussions", "users"
 end
